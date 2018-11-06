4e0213c0213aa12301126bf0f19d835d91d1042f
Kimberly Aviles <01010110.v@gmail.com>
2018-11-06 23:50:23 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,37 +1,10 @@
-def square_array(array)
-  array.each do |x|
-    x ** 2
-
-  end
-end
-
-
-=begin
-
-
-count the amount of integers in the given array
-  array_spots.count = 4
-turn that integer amount into workable indices for the array
-  array[0-3]
-  array[0], array[1], array[2], array[3]
-multiply each array spot to itself, subtracting 1 as it goes
-also assign the new value to old spot
-  array[3] = array[3] ** 2      counter = 4
-  array[2] = array[2] ** 2      counter -= 1
-  array[1] = array[1] ** 2
-  array[0] = array[0] ** 2      counter = 1
-
 
 
 def square_array(array)
-  counter = array.count
-  index = counter - 1
-
-  until counter = 0
-    array[index]
-    counter -= 1
-  end
+  array2 = []
+    array.each do |x|
+      x = x * x
+      array2 << x
+    end
+  return array2
 end
-
-
-=end
